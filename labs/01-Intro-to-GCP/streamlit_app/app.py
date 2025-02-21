f91ffb9ab286dfa41b38e88ab2d0d834cdf4b336
albaa <alba.arnau.alemany@gmail.com>
2025-02-21 19:07:58 +0100

    added URLS

--- labs/01-Intro-to-GCP/streamlit_app/app.py
+++ labs/01-Intro-to-GCP/streamlit_app/app.py
@@ -5,9 +5,10 @@ import requests
 
 def fetch_movie_details():
     # URL of your deployed Google Cloud Function
-    cloud_function_url = 'YOUR_GCP_URL_FUNCTION_1'
+    cloud_function_url = 'https://function1-853791711058.europe-west6.run.app'
     
     try:
+        print("accessed fetch movie details")
         # Make a request to the Google Cloud Function
         response = requests.get(cloud_function_url)
         response.raise_for_status()
@@ -21,6 +22,7 @@ def fetch_movie_details():
         st.error(f"Error making API request: {e}")
         return None
 def main():
+    print("accessed main")
     # Fetch movie details from the Google Cloud Function
     movie_details = fetch_movie_details()
     st.title("Movies from the TMDB Dataset")
@@ -31,7 +33,7 @@ def main():
         st.text_input("Enter a Movie ID to search:", key='search_movie_id')
         if st.button("Search"):
             search_movie_id = st.session_state.search_movie_id
-            search_url = f'YOUR_GCP_URL_FUNCTION_2?movie_id={search_movie_id}'
+            search_url = f'https://function2-853791711058.europe-west6.run.app?movie_id={search_movie_id}'
             try:
                 search_response = requests.get(search_url)
 
